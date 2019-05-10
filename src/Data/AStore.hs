e54e1dd975398aee45ffff929b403a74d797acf4
Mateusz Curylo <mhcurylo@gmail.com>
2019-05-10 05:44:07 +0100

    AStore IO implementation - types add up

--- src/Data/AStore.hs
+++ src/Data/AStore.hs
@@ -1,11 +1,21 @@
 module Data.AStore where
 
+import Prelude hiding (lines)
 import Conduit
+import Control.Concurrent
 import Control.Concurrent.STM.TBMChan
 import Control.Monad.State
+import Control.Monad.STM
+import Data.Text (pack, unpack)
+import Data.Conduit.Text (encode, decode, utf8, lines)
+import Data.Conduit.TMChan (sourceTBMChan)
 import Data.List (reverse)
 
-newtype Commit m a = Commit { commit :: a -> m () }
+data Commit m a where
+  Commit :: Monad m => (a -> m ()) -> Commit m a
+
+unCommit :: Commit m a -> (a -> m ())
+unCommit (Commit f) = f
 
 class Monad m => AStore m a where
   initAStoreCommit :: m (Commit m a)
@@ -15,5 +25,19 @@ instance AStore (State [a]) a where
   initAStoreCommit = return $ Commit (\a -> modify (a :))
   foldAStore f b = get >>= return . foldl f b . reverse
 
-
+instance forall a . (Show a, Read a) => AStore IO a where
+  initAStoreCommit = do
+    chan <- atomically $ newTBMChan 1000 
+    _ <- forkIO $ runConduitRes
+                $ sourceTBMChan chan
+                .| mapC (pack . show)
+                .| encode utf8
+                .| sinkFile "~/.ordning_history"
+    return $ Commit $ atomically . writeTBMChan chan
+  foldAStore f b = runConduitRes 
+               $ sourceFile "~/.ordning_history"
+               .| decode utf8
+               .| lines
+               .| mapC (read . unpack)
+               .| foldlC f b
 
